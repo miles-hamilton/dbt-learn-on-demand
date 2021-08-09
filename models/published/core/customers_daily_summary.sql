73e7c4efffa49962460952b3ff98c2d814a8eaee
alvin-mwangi <alvin.mwangi@gmail.com>
2021-08-09 10:23:05 -0700

    fix group by, alias

--- models/published/core/customers_daily_summary.sql
+++ models/published/core/customers_daily_summary.sql
@@ -4,7 +4,7 @@ select
     {{ dbt_utils.surrogate_key(['customer_id', 'order_date']) }} as id,
     customer_id, 
     order_date, 
-    count(*) 
+    count(*) as total_count
 
 from {{ ref('stg_orders') }}
-group by id, customer_id
+group by id, customer_id, order_date
